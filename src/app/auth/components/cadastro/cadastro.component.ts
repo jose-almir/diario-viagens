c176613f6dff77bc1c900a15d2801113566a91c7
veltonnunes <eriveltonnunes14@gmail.com>
2022-06-03 19:57:19 -0300

    Criação do link para a home-page

--- src/app/auth/components/cadastro/cadastro.component.ts
+++ src/app/auth/components/cadastro/cadastro.component.ts
@@ -19,6 +19,7 @@ export class CadastroComponent implements OnInit {
     {
       nome: ['', [Validators.required]],
       nick: ['', [Validators.required]],
+      recaptcha: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       senha: ['', [Validators.required, Validators.minLength(8)]],
       confirma_senha: [''],
