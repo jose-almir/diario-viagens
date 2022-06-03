6ccfd36dcc8e926fc0ee61c6c53c9bef4f005d86
Danrleydev <danrleysouza53@gmail.com>
2022-06-03 10:58:14 -0300

    Mudança de credenciais

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebase: {
-    projectId: 'diario-viagens-6c73b',
-    appId: '1:167645777431:web:a78fb2a6737b6bc1345652',
-    storageBucket: 'diario-viagens-6c73b.appspot.com',
+    projectId: 'diario-app-1955c',
+    appId: '1:520609709883:web:874bbb41dc63d6d086011e',
+    storageBucket: 'diario-app-1955c.appspot.com',
     locationId: 'us-central',
-    apiKey: 'AIzaSyBGTgkqJHUv1O_bWq7EfAJ1eUUnegEDFnw',
-    authDomain: 'diario-viagens-6c73b.firebaseapp.com',
-    messagingSenderId: '167645777431',
+    apiKey: 'AIzaSyAq3bo0jEB4NSzOj6mS9GaRTBvuFvevosM',
+    authDomain: 'diario-app-1955c.firebaseapp.com',
+    messagingSenderId: '520609709883',
   },
   production: false,
 };
