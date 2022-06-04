b01146bb6e43ffdcac49b05be92670e923694c41
jminervino <joaovictor_minervino@hotmail.com>
2022-06-04 16:32:21 -0300

    Mudança de icone e cor do check do upload

--- src/app/diarios/components/diario-add/diario-add.component.ts
+++ src/app/diarios/components/diario-add/diario-add.component.ts
@@ -8,17 +8,21 @@ import { Diario } from 'src/app/core/models/diario';
   styleUrls: ['./diario-add.component.scss'],
 })
 export class DiarioAddComponent implements OnInit {
-  diario: Diario = {} as Diario;
-  imagem?: File;
 
-  setImage(ev: any) {
-    this.imagem = ev.target.files[0];
-  }
+  diario: Diario = {} as Diario;
+  img?: File;
+  icone: String = "upload"
 
   constructor(private ref: MatDialogRef<DiarioAddComponent>) {}
 
+  setImage(ev: any) {
+    this.img = ev.target.files[0];
+    document.querySelector(".btnCheck")?.classList.add("btnCheck2");
+    this.icone = "library_add_check"
+  }
+
   onSubmit() {
-    this.ref.close({ diario: this.diario, imagem: this.imagem });
+    this.ref.close({ diario: this.diario, imagem: this.img });
   }
 
   ngOnInit(): void {}
