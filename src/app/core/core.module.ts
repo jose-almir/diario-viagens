581ee460a4ebe61e5179f9cce43feebca18a8502
im-ivan <ivanhfc00@gmail.com>
2022-06-03 22:02:25 -0300

    Criação de página de termos de uso

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MaterialModule } from '../shared/material.module';
 import { RouterModule } from '@angular/router';
+import { TermosDeUsoComponent } from './components/termos-de-uso/termos-de-uso.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    TermosDeUsoComponent
   ],
   imports: [
     CommonModule,
