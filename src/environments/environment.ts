604566a27ff731a26edff3ccfca4cb792bbca9d1
Luiz Henrique Burgos <henriquerburgos@gmail.com>
2022-06-05 15:54:51 -0300

    Comit de reparação de Firevbase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebase: {
-    projectId: 'diario-viagens-6c73b',
-    appId: '1:167645777431:web:a78fb2a6737b6bc1345652',
-    storageBucket: 'diario-viagens-6c73b.appspot.com',
+    projectId: 'diario-viagem',
+    appId: '1:404275993991:web:0fb822eb7f2930c18b8722',
+    storageBucket: 'diario-viagem.appspot.com',
     locationId: 'us-central',
-    apiKey: 'AIzaSyBGTgkqJHUv1O_bWq7EfAJ1eUUnegEDFnw',
-    authDomain: 'diario-viagens-6c73b.firebaseapp.com',
-    messagingSenderId: '167645777431',
+    apiKey: 'AIzaSyA8fL1oBlut5cLrnd1R9KMkBmU45afmU-k',
+    authDomain: 'diario-viagem.firebaseapp.com',
+    messagingSenderId: '404275993991',
   },
   production: false,
 };
